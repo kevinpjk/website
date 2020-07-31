5f053d56d0a3059df252153e0fbb2945f1043225
kevinpjk <kevinpjk@umich.edu>
2020-07-31 16:04:05 -0400

    Changed BrowserRouter to HashRouter

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Home = () => {
+    return (
+        <div>
+            <h2>Home Page</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eget lorem quam. Suspendisse commodo vulputate nunc, sit amet iaculis lorem bibendum vel. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla ultricies convallis laoreet. Nulla feugiat mi vel lorem dictum pulvinar sed et diam. Integer vel nunc risus. In eget malesuada urna. Morbi ultricies bibendum velit, sit amet ultricies nunc hendrerit gravida. In lacus elit, rutrum id aliquam at, semper non augue. Nulla feugiat lorem aliquet sagittis scelerisque.</p>
+            <p>In hac habitasse platea dictumst. Pellentesque interdum, diam pulvinar feugiat porttitor, metus ipsum ultrices nisl, vitae rutrum lectus purus ut diam. Sed id risus sed eros congue ultrices. Proin euismod mattis mauris a tempor. Aliquam erat volutpat. Nam porttitor dapibus nisl, non euismod eros venenatis quis. In ut mollis eros. Curabitur hendrerit gravida eros sed rhoncus. Curabitur lobortis porta neque non mollis. Sed non est non metus faucibus blandit sed et neque. Vestibulum in justo faucibus, luctus lectus et, venenatis tellus. Vivamus non nibh efficitur, malesuada metus non, varius augue. Fusce congue vitae neque vel tempus.</p>
+            <p>Nam efficitur arcu sit amet tempor congue. Fusce sed malesuada dui, eget semper enim. Sed convallis felis neque, tempus condimentum lacus lobortis ut. Fusce ac orci sit amet sem lobortis porta id id tellus. Morbi consectetur ante at arcu consectetur ultricies. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Duis nulla tellus, semper et tortor eu, viverra ultrices risus. Vivamus mattis erat ut urna hendrerit, sit amet placerat augue commodo. Morbi tristique eros mi, vitae tincidunt nunc elementum et.</p>
+        </div>
+    )
+}
---
+++ src/components/Jumbotron.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Jumbotron as Jumbo, Container } from 'react-bootstrap';
+import styled from 'styled-components';
+import img from '../assets/background.jpeg';
+
+const Styles = styled.div `
+    .jumbo {
+        background: url(${img}) no-repeat fixed bottom;
+        background-size: cover;
+        color: #efefef;
+        height: 200px;
+        position: relative;
+        z-index: -3;
+    }
+
+    .overlay {
+        background-color: #000;
+        opacity: 0.6;
+        position: absolute;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        right: 0;
+        z-index: -2;
+    }
+`;
+
+export const Jumbotron = () => (
+    <Styles>
+        <Jumbo fluid className="jumbo">
+            <div className="overlay"></div>
+            <Container>
+                <h1>Welcome to my website!</h1>
+                <p>Learn more about me here</p>
+            </Container>
+        </Jumbo>
+    </Styles>
+)
