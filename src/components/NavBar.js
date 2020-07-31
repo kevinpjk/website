f12167f1a82e39df0bfd6681ed3913a0db281a21
kevinpjk <kevinpjk@umich.edu>
2020-07-31 16:49:05 -0400

    Changed NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Nav, Navbar, NavItem } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Styles = styled.div `
@@ -22,10 +23,10 @@ export const NavBar = () => (
             <Navbar.Brand href="/">Jiankai Pu</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-                <Nav classname="ml-auto" pullRight>
-                    <NavItem><Nav.Link href="/website">Home</Nav.Link></NavItem>
-                    <NavItem><Nav.Link href="/website/about">About</Nav.Link></NavItem>
-                    <NavItem><Nav.Link href="/website/contact">Contact</Nav.Link></NavItem>
+                <Nav pullRight classname="ml-auto">
+                    <NavItem><NavLink to="/website">Home</NavLink></NavItem>
+                    <NavItem><NavLink to="/website/about">About</NavLink></NavItem>
+                    <NavItem><NavLink to="/website/contact">Contact</NavLink></NavItem>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
