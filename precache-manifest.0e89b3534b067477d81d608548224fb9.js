ade6230d86f2d1b9b20bf2109e5eb08aeb15497c
kevinpjk <kevinpjk@umich.edu>
2020-07-31 15:44:32 -0400

    Updates

---
+++ precache-manifest.0e89b3534b067477d81d608548224fb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8692a8d598fe6e77ab867d8a031ca456",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "1021ff6689ffa7594073",
+    "url": "/website/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7e0bc9a2791692febb4f",
+    "url": "/website/static/js/2.03ab348e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/website/static/js/2.03ab348e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1021ff6689ffa7594073",
+    "url": "/website/static/js/main.73a52523.chunk.js"
+  },
+  {
+    "revision": "9c23e00f99dd7916e537",
+    "url": "/website/static/js/runtime-main.9b825fe6.js"
+  },
+  {
+    "revision": "b0415cedf4b3afe2496653f2a12fc06d",
+    "url": "/website/static/media/background.b0415ced.jpeg"
+  }
+]);
