65360823e55d1d0d6f67070684ff4522ac741aa4
kevinpjk <kevinpjk@umich.edu>
2020-08-05 16:16:35 -0400

    Include container class on home.js

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components';
+import { Container } from 'react-bootstrap';
 // TODO: a div with three columns on projects (endless runner, search engine, )
 // a div with three columns on interests
 const Styles = styled.div `
